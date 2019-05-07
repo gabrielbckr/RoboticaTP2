7423d6fedb7d9669d602266a51817c816d17b557
gabriel <gabriel.becker.59@gmail.com>
2019-05-07 12:05:49 -0300

    arquivos para modularizacao

--- src/ColorfulPotato/FuncoesTP2.cpp
+++
@@ -1,1 +0,0 @@
-#include "FuncoesTP2.hpp"
--- src/ColorfulPotato/FuncoesTP2.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCOES__H__
-#define FUNCOES__H__
-
-
-// calcula qual é a cor a partir dos parâmetros
-void CalculaCor(const int RED, const int GREEN, const int BLUE);
-
-#endif
